3417c2b915815ce062c657478e1772c84a455689
alejandro-vaz <alejandro.vaz.myt@gmail.com>
2024-09-22 12:10:33 +0200

    creado repositorio

---
+++ index.md
@@ -0,0 +1,10 @@
+# Índice
+1. Introducción
+2. Objetivos
+3. Actividades
+4. Recursos
+5. Plan de trabajo
+6. (Marketing)
+7. Beneficios para los estudiantes
+8. Evaluación del club
+9. Conclusión
---
+++ mdtodocx.py
@@ -0,0 +1,15 @@
+import pypandoc
+
+def markdown_to_word(input_md_file, output_word_file):
+    try:
+        # Convert markdown to word file
+        pypandoc.convert_file(input_md_file, 'docx', outputfile=output_word_file)
+        print(f"Conversion successful: {output_word_file}")
+    except Exception as e:
+        print(f"Error during conversion: {e}")
+
+if __name__ == "__main__":
+    string = input("File name (without file extension): ")
+    input_md_file = f'{string}.md'  # Replace with your markdown file path
+    output_word_file = f'{string}.docx'  # Replace with desired word output path
+    markdown_to_word(input_md_file, output_word_file)
